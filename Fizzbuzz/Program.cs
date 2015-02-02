3af6d82c38e06222782270df811cf2eab435b2f4
unknown <Mahesh@Mahesh.chilli.local>
2015-02-02 15:52:57 +0200

    Version 1

---
+++ Fizzbuzz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print();
+        }
+
+        public static void Print()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                bool three=false;
+                bool five=false ;
+                bool both=false;
+
+                if (i%3 == 0)
+                {
+                    three = true;
+                }
+                if (i%5 == 0)
+                {
+                    five = true;
+                }
+                if (three == true && five == true)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(three==true)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (five == true)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
